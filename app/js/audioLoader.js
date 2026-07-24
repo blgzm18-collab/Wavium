d0890e75cf53b63ef2f27d3747fa85ef0acd1226
blgzm18-collab <blgzm18@gmail.com>
2026-07-23 23:45:15 -0400

    Add audio loading functionality with AudioContext

--- app/js/audioLoader.js
+++ app/js/audioLoader.js
@@ -1,1 +1,8 @@
+const audioCtx = new AudioContext();
+let audioBuffer = null;
 
+async function loadAudio(file) {
+    const arrayBuffer = await file.arrayBuffer();
+    audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+}
