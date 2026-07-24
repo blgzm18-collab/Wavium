e976eac795b780d986747365be5a21766ede60ee
blgzm18-collab <blgzm18@gmail.com>
2026-07-23 23:45:26 -0400

    Add drawWaveform function to visualize audio buffer

--- app/js/waveform.js
+++ app/js/waveform.js
@@ -1,1 +1,20 @@
+function drawWaveform(buffer) {
+    const canvas = document.getElementById("canvas");
+    const c = canvas.getContext("2d");
 
+    const data = buffer.getChannelData(0);
+    const step = Math.floor(data.length / canvas.width);
+    const amp = canvas.height / 2;
+
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.beginPath();
+    c.moveTo(0, amp);
+
+    for (let i = 0; i < canvas.width; i++) {
+        const value = data[i * step];
+        c.lineTo(i, amp + value * amp);
+    }
+
+    c.strokeStyle = "#4af";
+    c.stroke();
+}
